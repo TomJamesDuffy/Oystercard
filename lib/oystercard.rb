ce4861f6c162acbfa8a556d243712cc92cd8c706
AlexMcCarroll <alexandra@mccarrollhk.com>
2018-02-26 15:09:15 +0000

    Step 4 done and dusted

---
+++ lib/oystercard.rb
@@ -0,0 +1,8 @@
+# This is the Oystercard class
+class Oystercard
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
+  subject(:oystercard) { Oystercard.new }
+
+  describe '#initialization' do # where the code being tested comes from
+    it 'should initialize with a balance of zero' do # what it should do
+      expect(subject.balance).to eq 0 # expectation
+    end
+  end
 end
